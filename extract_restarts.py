8c85cc693418f11654beddf5a2d891e98ec9fc29
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2017-01-23 11:16:46 +0000

    Code to extract restart files and rename ready for upload to the ancil file repository

---
+++ extract_restarts.py
@@ -0,0 +1,155 @@
+##############################################################################
+# Program  : extract_restarts.py
+# Author   : Sarah Sparrow
+# Created  : 17/01/2017
+# Purpose  : To extract and rename restart files from a simulation
+##############################################################################
+import sys,os,getopt,glob
+import numpy as np
+import zipfile
+
+# Define the data directory
+gws='/group_workspaces/jasmin/cssp_china/'
+ddir=gws+'wp1/lotus/cpdn/lotus/'
+odir=gws+'users/ssparrow01/'
+
+model_types=["global","coupled","nested"]
+
+prefixes=['atmos','region','ocean']
+
+class Vars:
+        #input command line variables
+        batch=0
+        data_dir=gws+'wp1/lotus/cpdn/lotus/'
+        out_dir=gws+'users/ssparrow01/actual_restarts/'
+        model_type='nested'
+	dry_run=False
+        pass
+
+
+##############################################################################
+
+
+def Usage():
+        print "Usage :\n"\
+	"	--batch=	batch number to extract restarts from\n"\
+        "	--data_dir=	location of the batch directory\n"\
+        "	--out_dir=	location to put the extracted restarts in\n"\
+        "	--model_type=	model type enter 'global', 'coupled' or 'nested'\n"\
+	"	--dry_run	do a dry run without extracting files"
+
+        sys.exit()
+
+
+##############################################################################
+
+
+def ProcessCommandLineOpts():
+
+        # Process the command line arguments
+        try:
+                opts, args = getopt.getopt(sys.argv[1:],'',
+                ['batch=','data_dir=','out_dir=','model_type=','dry_run'])
+
+                if len(opts) == 0:
+                        Usage()
+                for opt, val in opts:
+                        if opt == '--batch':
+                                Vars.batch=val
+                        elif opt == '--data_dir':
+                                Vars.data_dir=val
+                        elif opt =='--out_dir':
+                        	Vars.out_dir=val
+			elif opt == '--model_type':
+				if val in model_types:
+                                	Vars.model_type=val
+				else:
+					print "Please use a valid model type"
+				 	Usage()
+			elif opt=='--dry_run':
+				Vars.dry_run=True
+        except getopt.GetoptError:
+                Usage()
+
+##############################################################################
+def get_date(dstring):
+	start_year=int(dstring[:4])
+        start_mon=int(dstring[4:])
+        syr=str(start_year+1)
+        smon=str(start_mon)
+        sdate=syr+'-'+smon+'-01'
+        return sdate
+
+def extract_restarts():
+    path=Vars.data_dir+'batch_'+Vars.batch+'/successful/*/*_restart.zip'
+    print path
+    restart_zips=glob.glob(path)
+
+    restart_lines=[]
+	
+    for restart_zip in restart_zips:
+	rzip=zipfile.ZipFile(restart_zip,'r')
+	
+	file_name=restart_zip.split('/')[-1]
+	if Vars.model_type=='nested':
+		file_id_split=file_name.split('_')[1:4]
+		region=file_name.split('_')[2]
+		sdate=get_date(file_id_split[2])
+		file_id_split[2]=sdate
+		prefix_ids=[0,1]
+	if Vars.model_type=='global':
+		file_id_split=file_name.split('_')[1:4]
+		sdate=get_date(file_id_split[2])
+		file_id_split[2]=sdate
+		prefix_ids=[0]
+	if Vars.model_type=='coupled':
+		file_id_split=file_name.split('_')[1:3]
+		sdate=get_date(file_id_split[2])
+                file_id_split[2]=sdate
+		prefix_ids=[0,2]
+
+        file_id='_'.join(file_id_split)
+
+	try:
+		rzip.extractall(Vars.out_dir)
+        except:
+		pass
+
+	line=[]
+	for pid in prefix_ids:
+		old_file=prefixes[pid]+"_restart.day"
+		new_file=prefixes[pid]+'_restart_batch_'+Vars.batch+'_'+file_id
+    		if Vars.dry_run:
+			print Vars.out_dir
+			print old_file,new_file
+		else:
+			if os.path.isfile(Vars.out_dir+new_file) or os.path.isfile(Vars.out_dir+new_file+'.gz'):
+				print "Already extracted"
+			else:
+				try:
+					os.rename(Vars.out_dir+old_file,Vars.out_dir+new_file)
+				except:
+					if os.path.exists(Vars.out_dir+old_file):
+    						os.remove(Vars.out_dir+old_file)
+					pass
+		# Create the line for the csv file information
+		line.append(new_file)
+	restart_lines.append(','.join(line))
+    
+    # write out the csv file
+    if Vars.dry_run:
+	print restart_lines
+    else:
+    	f=open(Vars.out_dir+'batch_'+Vars.batch+'_restarts.csv','w')
+    	for rline in restart_lines:    
+		f.write(rline+'\n')
+				
+#Main controling function
+def main():
+    ProcessCommandLineOpts()
+    extract_restarts()
+    print 'Finished!'
+
+#Washerboard function that allows main() to run on running this file
+if __name__=="__main__":
+  main()
