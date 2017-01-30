7798158362464fa9353c98c67facaf8b7e7ecdbd
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2017-01-30 13:50:04 +0000

    update to use new xml header generation code

--- create_basic_attribution_xml.py
+++ create_basic_attribution_xml.py
@@ -2,18 +2,21 @@
 
 # Program : create_template.py
 # Author  : Peter Uhe, modified from script by Neil Massey
-# Updates : 26/10/16 Sarah Sparrow: Updated to includ flag for generic restarts or input csv.
+# Updates : 26/10/16 Sarah Sparrow: Updated to include flag for generic restarts or input csv.
 # Purpose : create the xml template for the weather@home experiment 
 #           This code will create a pair of historical and natural xmls
 
 import xml.dom.minidom as dom
+from xml.dom.minidom import getDOMImplementation
 from StringIO import StringIO
 from ANC import *
 import random
 import getopt,sys
+import lxml.etree
 random.seed(1)	# ensure reproducibility!
 
 from create_xml2_funcs import CreatePertExpts,AddBatchDict,remove_whitespace_nodes
+from create_header_funcs import *
 
 class Vars:
 	#input command line variables
@@ -54,14 +57,21 @@ def ProcessCommandLineOpts():
 
 ##############################################################################
 
-def make_experiment_xml(prefix,xml_doc,params,restarts,ic_start,ic_end,start_umid):
+def make_experiment_xml(prefix,xml_doc,params,restarts,ic_start,ic_end,start_umid,batch):
 	# Set start umid  
 	anc = ANC()
         anc.Start(start_umid) # next set
 
         ######## Make expts ##########
+	end_umid=CreatePertExpts(xml_doc,params,restarts,ic_start,ic_end,anc)
 
-        end_umid=CreatePertExpts(xml_doc,params,restarts,ic_start,ic_end,anc)
+
+	# Add the batch info
+	batch['first_start_year']=params['model_start_year']
+        batch['last_start_year']=params['model_start_year']
+        batch['umid_start']=start_umid
+        batch['umid_end']=end_umid
+        AddBatchDict(xml_doc,batch)
 
         ######## Write out the file ########
 	xml_filename=prefix + str(params['model_start_year']) + "_" +\
@@ -73,6 +83,10 @@ def make_experiment_xml(prefix,xml_doc,params,restarts,ic_start,ic_end,start_umi
         xml_doc.writexml(fh,newl='\n',addindent='\t')
 #xml_doc.writexml(fh)
         fh.close()
+	
+	doc = lxml.etree.parse(xml_filename)
+	count = doc.xpath('count(//experiment)')
+	print "Number of workunits: ",int(count)
 
 	
 ##############################
@@ -82,10 +96,10 @@ if __name__ == "__main__":
 
 	# Parameters that are the SAME for historical and natural simulations
 	base_params={}
-	base_params['model_start_year']=2013
+	base_params['model_start_year']=2015
 	base_params['model_start_month']=12
 	base_params['run_years']=0
-	base_params['run_months']=3
+	base_params['run_months']=7
 	base_params['file_solar']='solar_1985_2020'
 	base_params['file_volcanic']='volc_1985_2020'
 	base_params['file_sulphox']='oxi.addfa'
@@ -96,8 +110,8 @@ if __name__ == "__main__":
 
 	# Parameters specific to all historical simulations
 	hist_params['file_ozone']='ozone_rcp45_N96_2009_2020v2'
-	hist_params['file_sst']='final_ancil_2year_OSTIA_sst_2013-12-01_2015-12-30'
-	hist_params['file_sice']='final_ancil_2year_OSTIA_ice_2013-12-01_2015-12-30'
+	hist_params['file_sst']='ALLclim_ancil_14months_OSTIA_sst_2015-12-01_2017-01-30'
+	hist_params['file_sice']='ALLclim_ancil_14months_OSTIA_ice_2015-12-01_2017-01-30'
 	hist_params['file_so2dms']='so2dms_Ev5a_baseCLE_N96_2009_2020'
 	hist_params['file_ghg']='ghg_defaults'
 
@@ -105,48 +119,74 @@ if __name__ == "__main__":
 	# Parameters specific to all natural simulations
 	nat_params['file_ozone']='o3_n96_pers_1959_1999_2020'
 	nat_params['file_so2dms']='so2dms_prei_N96_1855_0000P'
-	nat_params['file_sst']='delta_ancil_2year_OSTIA_sst_2013-12-01_2015-12-30_MMM'
+	nat_params['file_sst']='NATclim_ancil_14months_OSTIA_sst_MMM_2015-12-01_2016-01-30'
 	nat_params['file_sice']='OSICE_natural_0000P'
 	nat_params['file_ghg']='file_ghg_1850'
 
 	# Set up the xml doc - Remember to check/alter the header info as required
-	if Vars.site=='dev':
-		template='templates/dev_site_header_template.xml'
-	elif Vars.site=='main':
-		template='templates/main_site_header_template.xml'
-	else:
-		template='templates/dev_site_header_template.xml'
-	xml_doc = dom.parse(template)
+	expt_types=["actual","natural"]
+	impl = getDOMImplementation()
+	# Set up doc
+        upload_loc="dev"
+        app_config="config_wah2.2_eas50.xml"
+	region=app_config.split("_")[2].split(".")[0]
+	
+        # define stash files in the order global,regional (or global only)
+        stash_files=["xaakm_global_basic_2016-04-18.stashc","xacxf_region_basic_2016-07-19_v5.stashc"]
+
+	# define the generic restarts
+	generic_restarts='xhjlya.start.0000.360.new, generic_start_eas50_aabbw'
+
+	# Otherwise supply a csv list of <global_restart>,<regional_restart> for each experiment and define the number of pairs of restarts.
+	existing_restarts={"act":'batch486_restarts.csv',"nat":'batch487_restarts.csv'}
 
 	# Set up number of initial conditions to use within the experiments
-	ic_start=0
-	ic_end = 3
+        ic_start=0
+        ic_end = 3
 
-	# Add in the restart files
-	if Vars.generic==True:
-		# If the generic flag is set use the generic restarts for all experiments
-		restarts_list_hist='xhjlya.start.0000.360.new, generic_start_eas50_aabbw'
-        	restarts_list_nat='xhjlya.start.0000.360.new, generic_start_eas50_aabbw'
-		n_restarts=1
-	else:
-		# Otherwise supply a csv list of <global_restart>,<regional_restart> for each experiment and define the number of pairs of restarts.
-		restarts_list_allforc='batch221_restarts_hist.csv'
-		restarts_list_nat='batch221_restarts_nat.csv'
-		n_restarts=52
-
-	# Set start umid		
-	start_umid_hist = "a000"
-	start_umid_nat = "n000"
+	filename_prefix='xmls/wu_wah2_'+region+'_'
 
 	print "Creating experiments... "
+	for expt_type in expt_types:
+		print expt_type.title()+" experiment"
+		if expt_type=="actual":
+			expt_params=hist_params.copy()
+			start_umid = "a000"
+		elif expt_type=="natural":
+			expt_params=nat_params.copy()
+			start_umid = "n000"
+		else: 
+			raise
+        	xml_doc = impl.createDocument(None, "WorkGen", None)
+        	root = xml_doc.documentElement
 
-	filename_prefix_hist='xmls/wu_wah2_hist_'
-	filename_prefix_nat='xmls/wu_wah2_nat_'
+	        make_header(xml_doc,Vars.site,upload_loc,app_config,stash_files)
+
+		# Add in the restart files
+		if Vars.generic==True:
+			# If the generic flag is set use the generic restarts for all experiments
+			restarts_list=generic_restarts
+			restart_info="generic"
+			desc_info="Spin-up"
+			n_restarts=1
+		else:
+			restarts_list=existing_restarts[expt_type[0:2]]
+			restart_info=restarts_list.split("_")[0]
+			desc_info="2nd generation"
+			n_restarts=52
+
+		# Add in batch tags:
+                batch={}
+                batch['name']=expt_type.title()+" WAH2 East Asia 50km "+str(expt_params["model_start_year"])
+                batch['desc']=desc_info+" "+expt_type+" WAH2 East Asia 50km "+str(expt_params["model_start_year"])
+                batch['owner']="Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>"
+                batch['tech_info']=str(ic_end-ic_start)+" initial condition perturbations per restart, "+str(n_restarts)+" restarts per year from "+restart_info+", forced by OSTIA SSTs and sea-ice"
+                batch['proj']='LOTUS'
 
 
-	######## Make expts ##########
-	make_experiment_xml(filename_prefix_hist,xml_doc,hist_params,restarts_list_hist,ic_start,ic_end,start_umid_hist)
-	make_experiment_xml(filename_prefix_nat,xml_doc,nat_params,restarts_list_nat,ic_start,ic_end,start_umid_nat)	
-
+		######## Make expts ##########
+		make_experiment_xml(filename_prefix+expt_type+"_",xml_doc,expt_params,restarts_list,ic_start,ic_end,start_umid,batch)
+	
+		
 	print 'Done!'
 	print 'Now please verify the header/batch information before upload'
